f05a0ac1bebdf2e57e30c4256c1d39d6adf711a4
Martina J:son Gren <martinajsongren@hotmail.se>
2019-02-06 21:07:33 +0100

    Created setup method for ArraySet

--- src/test/java/ArraySet/ArraySetTest.java
+++ src/test/java/ArraySet/ArraySetTest.java
@@ -1,10 +1,18 @@
 package ArraySet;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArraySetTest {
+
+    private ArraySet arraySet;
+
+    @BeforeEach
+    void setup(){
+        arraySet = new ArraySet();
+    }
 
     @Test
     void add() {

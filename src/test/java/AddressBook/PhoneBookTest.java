d219ca3a6514a4d9c5fc4a6736fd9e713049ea0b
Martina J:son Gren <martinajsongren@hotmail.se>
2019-05-06 23:01:18 +0200

    Create setup method for Phonebook

--- src/test/java/AddressBook/PhoneBookTest.java
+++ src/test/java/AddressBook/PhoneBookTest.java
@@ -1,11 +1,29 @@
 package AddressBook;
 
+import ArraySet.ArraySet;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class PhoneBookTest {
+    PhoneBook phoneBook;
+    @Spy
+    ArraySet<Person> arraySet;
 
+    Person person;
+
+    @BeforeEach
+    void setupPhoneBook() {
+        phoneBook = new PhoneBook(arraySet);
+        person = new Person("Svampbob Fyrkant", "Snäckgatan 124","svampbob.fyrkant@krangliga-krabban.net", "0734445566", 25, new Date());
+    }
     @Test
     void addContact() {
     }
